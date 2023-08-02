fa044f4a328af3465a73281464f3bc5aed9e3230
Tracy Sonyah <sonyahtracy@gmail.com>
2023-08-02 03:06:14 +0000

    almost there

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const navigationMenu = document.querySelector('.navi');
 
 burgerMenu.addEventListener('click', function() {
   navigationMenu.classList.toggle('toggleNav');
+  // navigationMenu.style.display = 'block';
+  // navigationMenu.style.marginRight = '300px'
+
   console.log('hello')
 })
 
 document.addEventListener('scroll', function() {
-  navigationMenu.style.display = 'none';
-  // navigationMenu.style.display = 'block'
+  // navigationMenu.style.display = 'none';
 })
