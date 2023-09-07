074887f6af9a717424db71980ac0c8ff12ec1328
Tracy Sonyah <sonyahtracy@gmail.com>
2023-09-07 23:55:08 +0000

    done

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const navigationMenu = document.querySelector('.navi');
 
 burgerMenu.addEventListener('click', function() {
   navigationMenu.classList.toggle('toggleNav');
-  navigationMenu.style.left = '0';
+  navigationMenu.style.left = '0px';
+  navigationMenu.style.top = '0px';
+  navigationMenu.style.height = '20px';
   navigationMenu.style.backgroundColor = 'transparent';
   navigationMenu.style.backdropFilter = 'blur(5px)';
   navigationMenu.style.height = '400px'
